4cb765b0e3870784fcbf0661f1bdd223cc9649f9
Giuseppe Ambrosio <giuambro97@gmail.com>
2024-11-30 03:20:04 +0100

    [ADD] py leetcode 3255

---
+++ python/leetcode/3255.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        res = []
+        l = 0
+        nconsec = 1 # a sub array with one element have one consecutive element
+        for r in range(n):
+            if r > 0 and nums[r-1] + 1 == nums[r]:
+                nconsec += 1
+
+            if r - l + 1 > k:
+                if nums[l] + 1 == nums[l+1]:
+                    nconsec -= 1
+                l += 1
+            
+            if r - l + 1 == k:
+                res.append(nums[r] if nconsec == k else -1)
+        
+        return res
