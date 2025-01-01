523b08ef4d3c4654fc739124744bafcc857b6651
Giuseppe Ambrosio <giuambro97@gmail.com>
2025-01-01 21:11:43 +0100

    [CHANGE] py leetcode 162

--- python/leetcode/162.py
+++ python/leetcode/162.py
@@ -1,28 +1,21 @@
+import math
 from typing import List
-
-def b_search(nums: List[int], l: int, r: int) -> int:
-    m = (l + r) // 2
-
-    prev = nums[m-1] if m - 1 >= 0 else float("-inf")
-    succ = nums[m+1] if m + 1 < len(nums) else float("-inf")
-
-    if prev < nums[m] and nums[m] > succ:
-        return m
-    elif prev < nums[m] < succ:
-        return b_search(nums, m + 1, r)
-    elif prev > nums[m] > succ:
-        return b_search(nums, l, m - 1)
-    else:
-        return b_search(nums, l, m - 1)
-    
-
 
 
 class Solution:
-
     def findPeakElement(self, nums: List[int]) -> int:
-        return b_search(nums, 0, len(nums) - 1)
-    
+        def bs(l: int, r: int):
+            m = l + ((r-l) // 2)
 
-if __name__ == '__main__':
-    print(Solution().findPeakElement([1,3,1,2]))
+            vm = nums[m]
+            vml = nums[m-1] if m > 0 else -math.inf 
+            vmr = nums[m+1] if m < len(nums) - 1 else -math.inf
+
+            if vm > vmr:
+                if vm > vml:
+                    return m
+                return bs(0, m-1)
+            else:
+                return bs(m+1, r)
+
+        return bs(0, len(nums)-1)
