3433f3393e516da13808fb01b243f39c6915ce29
Giuseppe Ambrosio <giuambro97@gmail.com>
2025-01-25 15:07:28 +0100

    [ADD] py leetcode 2948

--- python/leetcode/2948.py
+++ python/leetcode/2948.py
@@ -1,34 +1,27 @@
-from collections import deque
+from collections import defaultdict, deque
 from typing import List
 
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         n = len(nums)
-        if n == 1:
-            return nums
-        
+
         snums = sorted(nums)
-        buckets = {}
-        num_to_bucket = {}
-        num_buckets = 0
+        buckets = defaultdict(deque)
+        buckets[0].appendleft(snums[0])
+        num_to_bucket = {snums[0]: 0}
 
-        l = 0
-        for r in range(1,n+1):
-            if r == n or snums[r] - snums[r-1] > limit:
-                buckets[num_buckets] = deque(snums[l:r])
-                for a in snums[l:r]:
-                    num_to_bucket[a] = num_buckets
-                num_buckets += 1
-                l = r
+        for r in range(1,n):
+            if snums[r] - snums[r-1] > limit:
+                num_to_bucket[snums[r]] = len(buckets)
+                buckets[len(buckets)].append(snums[r])
+            else:
+                buckets[len(buckets)-1].append(snums[r])
+                num_to_bucket[snums[r]] = len(buckets) - 1
 
-        buckets[num_buckets] = deque(snums[l:r+1])
-        for a in snums[l:r+1]:
-            num_to_bucket[a] = num_buckets
-        
         return [buckets[num_to_bucket[a]].popleft() for a in nums]
 
 
 if __name__ == '__main__':
-    nums = [1,7,6,18,2,1]
-    limit = 3
+    nums = [1,5,3,9,8]
+    limit = 2
     Solution().lexicographicallySmallestArray(nums, limit)
