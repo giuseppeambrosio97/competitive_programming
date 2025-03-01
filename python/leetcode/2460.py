a5f60f422cd2432aeb733531b8fac42d513d874d
Giuseppe Ambrosio <giuambro97@gmail.com>
2025-03-01 01:24:29 +0100

    [ADD] py leetcode 2460

--- python/leetcode/2460.py
+++ python/leetcode/2460.py
@@ -8,15 +8,9 @@ class Solution:
             if nums[i] == nums[i+1]:
                 nums[i] = 2*nums[i]
                 nums[i+1] = 0
-        l = 0
-        while l < n:
-            if nums[l] == 0:
-                r = l + 1
-                while r < n and nums[r] == 0:
-                    r += 1
-                if r >= n:
-                    break
-                nums[l], nums[r] = nums[r], nums[l]
-            l += 1
+        idx = 0
+        for i in range(n):
+            if nums[i]:
+                nums[idx], nums[i] = nums[i], nums[idx]
+                idx+=1
         return nums
-
