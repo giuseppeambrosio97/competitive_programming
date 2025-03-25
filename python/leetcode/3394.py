bfb0776ec2ac0faf67f23db170b95b51b3877eda
Giuseppe Ambrosio <giuambro97@gmail.com>
2025-03-25 11:38:20 +0100

    [ADD] py leetcode 3394 - Implement solution for checkValidCuts problem

---
+++ python/leetcode/3394.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
+        x = sorted([(r[0], r[2]) for r in rectangles])
+        y = sorted([(r[1], r[3]) for r in rectangles])
+
+        def cnt_non_overlap(arr):
+            cnt = 0
+            cend = arr[0][1]
+            for start, end in arr[1:]:
+                if start >= cend:
+                    cnt += 1
+                cend = max(cend, end)
+            cnt += 1
+            return cnt
+        
+        return max(cnt_non_overlap(x), cnt_non_overlap(y)) >= 3
+
+if __name__ == "__main__":
+    n = 5
+    rectangles = [[0,2,2,4],[1,0,3,2],[2,2,3,4],[3,0,4,2],[3,2,4,4]]
+    Solution().checkValidCuts(n, rectangles)
