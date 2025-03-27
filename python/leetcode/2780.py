6de4e21fab6a595c1fd2902d7d50c8a59c7bdd9e
Giuseppe Ambrosio <giuambro97@gmail.com>
2025-03-27 18:35:05 +0100

    [ADD] py leetcode 2780 - Implement solution for minimumIndex problem

---
+++ python/leetcode/2780.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        # Find the dominant element in the entire array
+        freq = defaultdict(int)
+        dominant, max_count = -1, -1
+
+        for num in nums:
+            freq[num] += 1
+            if freq[num] > max_count:
+                dominant, max_count = num, freq[num]
+
+        left_count, right_count = 0, max_count
+
+        # Iterate and find the split point
+        for i, num in enumerate(nums):
+            if num == dominant:
+                left_count += 1
+                right_count -= 1
+
+            if left_count > (i + 1) // 2 and right_count > (len(nums) - (i + 1)) // 2:
+                return i
+        
+        return -1
