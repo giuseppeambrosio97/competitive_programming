0d5d511bf1e522309d3b38f842b08fbadac6d80f
Giuseppe Ambrosio <giuambro97@gmail.com>
2025-06-23 23:01:25 +0200

    ADD py leetcode 2081 - Implement solution for kMirror function

---
+++ python/leetcode/2081.py
@@ -0,0 +1,35 @@
+class Solution:
+    def kMirror(self, k: int, n: int) -> int:
+        def isPalindrome(x: int, base: int) -> bool:
+            digit = list()
+            while x:
+                digit.append(x % base)
+                x //= base
+            return digit == digit[::-1]
+
+        left, cnt, ans = 1, 0, 0
+        while cnt < n:
+            right = left * 10
+            # op = 0 indicates enumerating odd-length palindromes
+            # op = 1 indicates enumerating even-length palindromes
+            for op in [0, 1]:
+                # enumerate i'
+                for i in range(left, right):
+                    if cnt == n:
+                        break
+                    combined = i
+                    x = i // 10 if op == 0 else i
+                    while x:
+                        combined = combined * 10 + x % 10
+                        x //= 10
+                    if isPalindrome(x=combined,base=k):
+                        cnt += 1
+                        ans += combined
+            left = right
+
+        return ans
+
+if __name__ == "__main__":
+    k = 2
+    n = 5
+    print(Solution().kMirror(k,n))
