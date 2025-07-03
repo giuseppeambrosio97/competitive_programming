abec4f8244af5f00af13759ca63eaca16f906d08
Giuseppe Ambrosio <giuambro97@gmail.com>
2025-07-03 10:48:33 +0200

    REFINE py leetcode 3304 - Optimize kthCharacter function implementation

--- python/leetcode/3304.py
+++ python/leetcode/3304.py
@@ -1,16 +1,10 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        ss = "a"
-
         next_ch = lambda ch: chr(97+((ord(ch)-97)+1)%26)
-
-        while len(ss) < k:
-            rr = []
-            for si in ss:
-                rr.append(next_ch(si))
-            ss += "".join(rr)
-        
-        return ss[k-1]
+        s = "a"
+        while len(s) < k:
+            s += "".join(next_ch(si) for si in s)
+        return s[k-1]
 
         
 if __name__ == "__main__":
