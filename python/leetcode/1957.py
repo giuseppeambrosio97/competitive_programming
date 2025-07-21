aa0e1c24dcf8bcb2f20197cb320f812eae4ddcfa
Giuseppe Ambrosio <giuambro97@gmail.com>
2025-07-21 10:57:29 +0200

    REFAC py leetcode 1957 - Simplify makeFancyString function by removing the fancy_check helper and implementing a direct frequency check for characters

--- python/leetcode/1957.py
+++ python/leetcode/1957.py
@@ -3,20 +3,30 @@ from collections import Counter
 
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        def fancy_check(i: int, ts: str) -> bool:
-            """True the s[i] char can be added. False otherwise"""
-            if i <= 1 or len(ts) <= 1:
-                return True
-            _most_common = Counter(ts[-2:]).most_common(1)[0]
+        # def fancy_check(i: int, ts: str) -> bool:
+        #     """True the s[i] char can be added. False otherwise"""
+        #     if i <= 1 or len(ts) <= 1:
+        #         return True
+        #     _most_common = Counter(ts[-2:]).most_common(1)[0]
+        #     return _most_common[0] != s[i] or _most_common[1] < 2
+        # ts = ""
+        # for i, ch in enumerate(s):
+        #     if fancy_check(i, ts):
+        #         ts += ch
+        # return ts
+        prev = s[0]
+        freq = 1
+        res = ""
 
-
-            return _most_common[0] != s[i] or _most_common[1] < 2
-
-        ts = ""
-        for i, ch in enumerate(s):
-            if fancy_check(i, ts):
-                ts += ch
-        return ts
+        for i in range(1, len(s)):
+            if s[i] == prev:
+                freq += 1
+            else:
+                freq = 1
+                prev = s[i]
+            if freq < 3:
+                res += s[i]
+        return res
 
 
 
