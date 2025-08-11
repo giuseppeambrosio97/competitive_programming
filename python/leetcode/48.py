ecd3e20a80f3db8fee3057a218062c1eb58d24bb
Giuseppe Ambrosio <giuambro97@gmail.com>
2025-08-12 01:20:56 +0200

    ADD py leetcode 48 - Implement rotate function to rotate a matrix in-place

---
+++ python/leetcode/48.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l, r = 0, len(matrix) - 1
+
+        while l < r:
+            for i in range(r - l):
+                t, b = l, r
+
+                tmp = matrix[t][l + i]
+
+                matrix[t][l + i] = matrix[b - i][l]
+                matrix[b - i][l] = matrix[b][r - i]
+                matrix[b][r - i] = matrix[t + i][r]
+                matrix[t + i][r] = tmp
+            l += 1
+            r -= 1
+
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    Solution().rotate(matrix)
+    print(matrix)
